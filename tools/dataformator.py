b5a75bd3541815a7f9077a730ca25e88ef3f36f3
bingkuo.chen <bingkuo.chen@focus-ebeam.com>
2017-08-19 13:45:56 +0800

    Implement dataformater.py script
    scorce json data is generated by newhouse.py
    
    target json data will saved to script placed directory
    target sjon file format is supported by baidu api, need to copy or read by
    javascript
    
    Change-Id: Ifaf2758cb2db192dea83e83e79b35d900f26d0da

--- tools/dataformator.py
+++ tools/dataformator.py
@@ -8,8 +8,12 @@ source data is come from spider
 '''
 
 import json
+import os
 from urllib2 import urlopen
 from urllib2 import quote
+import types
+
+saveJsonFormat = { "houseName":"Null", "averagePrice":0, "lng":0, "lat":0, "count":0}
 
 def load(filePath):
     try:
@@ -20,25 +24,74 @@ def load(filePath):
         print("open file failed, please recheck file path or permission!")
 
 def getPosition(address):
+    ''' return type is str '''
     url = 'http://api.map.baidu.com/cloudgc/v1?'
     ak = '8ZxthUScD9ktwOwjNRwg4xN6BCTm6qeC'
+    address = address.encode('utf-8')
     address = quote(address)
     url = url + 'ak=' + ak + '&address=' + address
-    requestData = urlopen(url)
-    print(requestData)
-    requestData = requestData.read().decode("UTF-8")
-    temp = json.load(requestData)
-    return temp
+    requestData = urlopen(url).read()
+    return requestData
+
+def toHotMapValue(averagePrice):
+    ''' this method mainly to convert  averagePrice to hotMap value'''
+    ''' hotMap value range: 0-255'''
+    ''' average price range 1000 - 30000'''
+    ''' factor  = 1 / ((200000 - 1000) / 255) '''
+
+    factor = 1 / 780.3921
+    if averagePrice <= 1000:
+        return 0
+    elif averagePrice >= 200000:
+        return 255
+    return factor * averagePrice
 
 if __name__ ==  '__main__':
+
+    scriptDir  = os.path.dirname(__file__)
+    scriptPath = scriptDir + '\\' + 'newhouse.json'
 
     testPath = 'D:/web/newhouse.json'
     result = load(testPath)
 
-    for i in result:
-        strData = i['houseAddr']
-        print(strData)
-        getPosition('KFC')
-        exit(0)
+    with open(scriptPath, 'w') as output:
+        output.write('[' + '\n')
+        index = 0
+        contentCount = len(result)
+        for i in result:
+            print("writing  newhouse.json file...........{}".format(index))
+            strData = i['houseAddr']
+            print(u'%s' % strData)
+            posInfo = getPosition(u'%s' % strData)
+            houseName = i['houseName']
+            houseAveragePrice = i['averagePrice']
+            # if type(houseAveragePrice) is types.NoneType:
+            #     houseAveragePrice = 0
 
-print("********************************************************")
+
+            posInfo = json.loads(posInfo)
+            print(posInfo)
+            print("**************************************")
+            houseLongitude = posInfo['result'][0]['location']['lng']
+            houseLatitude = posInfo['result'][0]['location']['lat']
+            if type(houseAveragePrice) is types.NoneType:
+                houseWeight = 0
+            else:
+                houseWeight = toHotMapValue(int(houseAveragePrice))
+
+            saveJsonFormat['houseName'] = houseName.encode('utf-8')
+            saveJsonFormat['averagePrice'] = houseAveragePrice
+            saveJsonFormat['lng'] = houseLongitude
+            saveJsonFormat['lat'] = houseLatitude
+            saveJsonFormat['count'] = houseWeight
+            print(saveJsonFormat)
+            print(type(saveJsonFormat))
+            json.dump(saveJsonFormat, output, ensure_ascii = False)
+            if  index + 1 < contentCount:
+                output.write(',')
+            index += 1
+            output.write('\n')
+        output.write(']' + '\n')
+
+print("json file write successfully!")
+print("***************************************************************************")
